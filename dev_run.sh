e0814da7dd725b8ee500b9406c9026286af6dd8d
princekx <prince.xavier@gmail.com>
2026-06-06 00:03:06 +0100

    Add Cylc configuration files for local and SPICE/SLURM execution environments

--- dev_run.sh
+++ dev_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=dev_run
-#SBATCH --output=logs/dev_run_%j.out
-#SBATCH --error=logs/dev_run_%j.err
+#SBATCH --output=/usr/tmp/logs/dev_run_%j.out
+#SBATCH --error=/usr/tmp/logs/dev_run_%j.err
 #SBATCH --time=1:00:00
 #SBATCH --mem=80gb
 #SBATCH --cpus-per-task=8
